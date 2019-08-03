7bd1db544ee30f0f1aac87a70cf17d9c693b221a
DomiDre <homiedomi@gmail.com>
2019-08-03 18:01:06 +0200

    Add comments, fix data loading

--- src/app/modules/generic/components/main/main.component.ts
+++ src/app/modules/generic/components/main/main.component.ts
@@ -21,16 +21,19 @@ class genericModel {
 })
 export class MainComponent implements OnInit {
 
-  
+  // for compatibility with rusfun code, store data in Float64Arrays
   x: Float64Array = new Float64Array([]);
   y: Float64Array = new Float64Array([]);
   yData: Float64Array = new Float64Array([]);
   syData: Float64Array = new Float64Array([]);
 
+  // FormGroup to control the linspace if no data is present
   linspaceForm: FormGroup;
+  // FormGroup to control the parameters of a set model 
   parameterForm: FormGroup;
 
-
+  // list of all available models, their parameters, default start values
+  // an description text and which function to call to calculate said model
   models: genericModel[] = [
     { 
       name: 'linear',
@@ -61,20 +64,25 @@ export class MainComponent implements OnInit {
       infoText: 'Function:\na*x^2 + b*x + c',
       setFunction: this.setParabola.bind(this)}
   ]
-
+  
+  // currently selected model
   selectedModel: genericModel;
+  
+  // selected data file
   selectedXYFile: Blob;
 
   constructor(
     private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    // at startup initialize the linspace from 0..1 with 10 steps
     this.linspaceForm = this.formBuilder.group({
       xMin: [0, [Validators.required]],
       xMax: [1, Validators.required],
       Nx: [10, Validators.required]
     }, { updateOn: 'blur' });
 
+    // on user input, and if a model is selected, recalculate the model
     this.linspaceForm.valueChanges
     .subscribe(val => {
       if(this.selectedModel && this.linspaceForm.valid) {
@@ -83,16 +91,15 @@ export class MainComponent implements OnInit {
     });
   }
 
-  
-
   modelSelected() {
+    // when the user selects a model, initalize the FormGroup for the parameters
     const paramGroup = {};
     for (const param of this.selectedModel.parameters) {
       paramGroup[param.name] = [param.value, Validators.required]
     }
     this.parameterForm = this.formBuilder.group(paramGroup, { updateOn: 'blur' });
 
-    // when parameters are changed externally
+    // when parameters are changed by the user
     // update internal parameters and update the plot
     this.parameterForm.valueChanges
     .subscribe(val => {
@@ -104,11 +111,14 @@ export class MainComponent implements OnInit {
         this.selectedModel.setFunction();
       }
     });
-    this.selectedModel.setFunction();
 
+    // calculate the model once after selection
+    this.selectedModel.setFunction();
   }
 
   calculate_linspace() {
+    // calculate the linspace for given xMin, xMax and number of steps
+    // only calculated if no data is selected
     if (this.yData.length === 0) {
       const x = [];
       const xMin = Number(this.linspaceForm.controls.xMin.value);
@@ -137,23 +147,52 @@ export class MainComponent implements OnInit {
   }
 
   loadXYData(xyFileInput) {
+    // loading 2 or 3 column files
     const fileList: FileList = xyFileInput.target.files;
     if ( fileList.length > 0 ) {
       this.selectedXYFile = xyFileInput.target.files[0];
-      // // get XY src for preview
       const reader = new FileReader();
       reader.onload = event => {
+        // read the data
         const content = reader.result;
         const lines = String(content).split('\n');
+
+        // initialize empty lists for the columns
         const x = [];
         const yData = [];
         const syData = [];
 
+        // check lines for first non-empty line that's not a comment and see
+        // if it has 2 or 3 columns
+        let has_three_cols = false;
         for (let line of lines) {
+          // ignore comments
+          if (line.trim().startsWith('#')) continue
+
+          // ignore empty lines
+          if (line.trim().length > 0) {
+            const splitted_line = line.split(/\s+/);
+            has_three_cols = splitted_line.length >= 3;
+          }
+        }
+        for (let line of lines) {
+          // ignore comments
+          if (line.trim().startsWith('#')) continue
+
+          // split line at white-spaces or tabs
           const splitted_line = line.split(/\s+/)
-          x.push(Number(splitted_line[0]));
-          yData.push(Number(splitted_line[1]));
-          syData.push(1);
+
+          if (splitted_line.length >= 2) {
+            x.push(Number(splitted_line[0]));
+            yData.push(Number(splitted_line[1]));
+            if (has_three_cols) {
+              if (splitted_line.length >= 3) {
+                syData.push(Number(splitted_line[2]));
+              } else {
+                throw "File identified as 3 column file has one line with only 2 columns"
+              }
+            }
+          }
         }
         this.linspaceForm.disable();
         this.x = new Float64Array(x);
